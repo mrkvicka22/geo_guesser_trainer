806552021164831dc8d12adc6cec930e692e4c4e
Matej <matej.novosad.mn@gmail.com>
2022-08-26 20:55:05 +0200

    Initial commit

--- geoguessr_trainer/src/App.jsx
+++ geoguessr_trainer/src/App.jsx
@@ -1,8 +1,30 @@
-import { useState } from 'react'
-import './App.css'
+import WelcomePage from './routes/WelcomePage';
+import { Tabs, TabList, TabPanels, TabPanel, Tab } from '@chakra-ui/react'
+import Guesser from './routes/Guesser'
+import React from 'react'
 
 function App() {
-  return <h1>Hello world</h1>
+
+  return (
+    <Tabs align="end" variant="soft-rounded">
+      <TabList>
+        <Tab>Home</Tab>
+        <Tab>Poles</Tab>
+        <Tab>Bollards</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel>
+          <WelcomePage/>
+        </TabPanel>
+        <TabPanel>
+          <Guesser type={"poles"} />
+        </TabPanel>
+        <TabPanel>
+          <Guesser type={"bollards"} />
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
+  );
 }
 
 export default App
---
+++ geoguessr_trainer/src/routes/Guesser.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Guesser({type}) {
+  return (
+    <div>Guesser {type}</div>
+  )
+}
